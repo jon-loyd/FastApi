a1a60c690adc4db280bee13b7c0eb1a83055563f
Jon Loyd <jonathanloydbusiness@gmail.com>
2025-12-18 17:00:28 -0500

    quick code cleanup

--- app/db/deps.py
+++ app/db/deps.py
@@ -1,6 +1,6 @@
-from app.db.session import SessionLocal
-from sqlalchemy.orm import Session
 from typing import Generator
+from sqlalchemy.orm import Session
+from app.db.session import SessionLocal
 
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
--- app/main.py
+++ app/main.py
@@ -1,15 +1,7 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.db.session import Base, engine
 from app.core.config import settings
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup code
-    yield
-    # Teardown code
-
-app = FastAPI(title=settings.app_name, lifespan=lifespan)
+app = FastAPI(title=settings.app_name)
 
 @app.get("/health")
 def health():
